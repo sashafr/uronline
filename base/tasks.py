a4e42d329783b9ebafd81935c8eb4388f26eb349
Sasha <sashafr@sas.upenn.edu>
2014-10-27 16:59:52 -0400

    fixed javascript error that forced Has Image boxed to always be checked on Object admin page

--- base/tasks.py
+++ base/tasks.py
@@ -1,9 +1,15 @@
 from __future__ import absolute_import
-
+from datetime import datetime, timedelta
 from celery import shared_task
 import urllib2
+from base.models import SubjectProperty, Subject
 
 @shared_task
 def update():
     response = urllib2.urlopen('http://localhost:8983/solr/subjects/dataimport?command=full-import').read()
     print response
+    
+@shared_task
+def precomp_object_fields():
+    time_threshold = datetime.now() - timedelta(hours=2)
+    updated_subjects = Subject.objects.filter(modified__gt=time_threshold)
